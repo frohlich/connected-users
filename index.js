af4e17f2d1ae6469b3849da0d27f8d1f53618122
Lucas Frohlich <lucasfrohlich@gmail.com>
2016-05-17 21:13:02 -0300

    Added todo list into file

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * TODO: 
+ *  1- Install and configure MEAN.JS
+ *  2- Use YOMEN to generete CRUDS:
+ *  2a- User(id,name,macaddress,hostname,)
+ *  2b- UserHistory(id, user_id, ip, loggedIn, loggedOut, longitude, latitude)
+ *  3- DO:
+ *  3a- connect: method used when the users connects into;
+ *  3b- refresh: method called automatic, it check into network if connected users are online.
+ *  3c- apply: method set update and download into transmission
+ *  4- Parameters:
+ *  4a- Networks stuffs: IP, Mask, 
+ *  4b- Transmissioon: IP, port, login, password.
+ *  4c- Others.
+ * 
+ */
